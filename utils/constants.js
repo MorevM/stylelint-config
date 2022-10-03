4cdcb139f3eaef7edb382ef295f8e19e48a0ab4d
MorevM <max.seainside@gmail.com>
2022-10-03 12:46:36 +0300

    refactor: Relint files after ESlint config update

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@
 const WARN = 'warning';
 const ERROR = 'error';
 const KEBAB_CASE =  /^([a-z][\da-z]*)(-[\da-z]+)*$/;
-const KEBAB_CASE_ALLOW_FIRST_UNDERSCORE =  /^_*?([a-z][\da-z]*)(-[\da-z]+)*$/;
+const KEBAB_CASE_ALLOW_FIRST_UNDERSCORE =  /^_*([a-z][\da-z]*)(-[\da-z]+)*$/;
 const KEBAB_CASE_ALLOW_DOUBLE = /^([a-z][\da-z]*)(-{1,2}[\da-z]+)*$/;
 
 module.exports = {
