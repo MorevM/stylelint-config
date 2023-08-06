8148ddecbfa8ba0237ba95f879a6323ac84e3bf0
MorevM <max.seainside@gmail.com>
2023-08-06 21:27:19 +0300

    feat(css): Take account of `order/properties-alphabetical-order` rule

--- configurations/css/plugins/order.js
+++ configurations/css/plugins/order.js
@@ -5,6 +5,7 @@ const { WARN } = require('../../../utils/constants.js');
 module.exports = {
 	plugins: ['stylelint-order'],
 	rules: {
+		'+order/properties-alphabetical-order': null,
 		'+order/properties-order': [propertiesOrder, { severity: WARN }],
 		'+order/order': [rulesOrder, { severity: WARN }],
 	},
