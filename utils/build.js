510f3202b722bca8b8eafabde05daf411608b992
MorevM <max.seainside@gmail.com>
2024-11-16 15:41:05 +0300

    chore: Do not write to `package.json` during `build` task
    Won't change anyway

--- utils/build.js
+++ utils/build.js
@@ -1,8 +1,7 @@
 import { dirname, resolve } from 'node:path';
 import util from 'node:util';
 import { fileURLToPath } from 'node:url';
-import { readFileSync, rmSync, mkdirSync, writeFileSync } from 'node:fs';
-import { ESLint } from 'eslint';
+import { rmSync, mkdirSync, writeFileSync } from 'node:fs';
 import { css, scss } from '../configurations/index.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -14,25 +13,13 @@ const CONFIGURATIONS = [
 
 const BUILD_DIRECTORY_RELATIVE = './build/configurations/';
 const BUILD_DIRECTORY = resolve(__dirname, '..', BUILD_DIRECTORY_RELATIVE);
-const PACKAGE_JSON_PATH = resolve(__dirname, '..', 'package.json');
-
-const eslint = new ESLint({ fix: true });
 
 rmSync(BUILD_DIRECTORY, { recursive: true, force: true });
 mkdirSync(BUILD_DIRECTORY, { recursive: true });
 
-let exportsField = {};
-
 const toExport = (source) =>
 	`export default ${util.inspect(source, { maxArrayLength: 9999, depth: 999 })}`
 		.replaceAll(/["'](import\(.*\))["']/g, '(await $1).default'); // unwrap imports
-
-const getNames = ({ name }) => {
-	const filename = `${name}.js`;
-	const exportsName = `./${name}`;
-
-	return { filename, exportsName };
-};
 
 CONFIGURATIONS.forEach((declaration) => {
 	const configuration = {
@@ -40,22 +27,8 @@ CONFIGURATIONS.forEach((declaration) => {
 		...declaration.configuration,
 	};
 
-	const { exportsName, filename } = getNames(declaration);
+	const filename = `${declaration.name}.js`;
 	const configSource = toExport(configuration);
-	exportsField[exportsName] = `${BUILD_DIRECTORY_RELATIVE}${filename}`;
 
 	writeFileSync(`${BUILD_DIRECTORY}/${filename}`, configSource);
 });
-
-// Main export aliasing `/configurations/scss`
-exportsField = {
-	'.': exportsField['./scss'],
-	...exportsField,
-};
-
-const packageJsonContents = JSON.parse(readFileSync(PACKAGE_JSON_PATH));
-packageJsonContents.main = exportsField['.'];
-packageJsonContents.exports = exportsField;
-
-writeFileSync(PACKAGE_JSON_PATH, JSON.stringify(packageJsonContents, null, '\t'));
-ESLint.outputFixes(await eslint.lintFiles('./package.json'));
