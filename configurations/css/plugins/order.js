dbf05eab2dc3755a4ad2ee4ced5c06984718a7e7
MorevM <max.seainside@gmail.com>
2026-04-10 18:41:09 +0300

    feat(css): Take account if `order/custom-properties-alphabetical-order` rule

--- configurations/css/plugins/order.js
+++ configurations/css/plugins/order.js
@@ -5,6 +5,7 @@ import { WARNING } from '../../../utils/constants.js';
 export default {
 	plugins: ['stylelint-order'],
 	rules: {
+		'+order/custom-properties-alphabetical-order': null,
 		'+order/properties-alphabetical-order': null,
 		'+order/properties-order': [propertiesOrder, { severity: WARNING }],
 		'+order/order': [rulesOrder, { severity: WARNING }],
