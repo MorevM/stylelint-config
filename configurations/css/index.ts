a9bf7cd6343a0217e2b87d2ed243d3b4093148f5
MorevM <max.seainside@gmail.com>
2026-06-27 22:49:42 +0300

    refactor: Get rid of `_settings.ts` files in favor of direct exports from a barrel file

--- configurations/css/index.ts
+++ configurations/css/index.ts
@@ -1,4 +1,37 @@
 import { processExports } from '../../utils/helpers.js';
-import { base, parts } from './_settings.js';
+import {
+	pluginDeclarationBlockNoIgnoredProperties,
+	pluginHighPerformanceAnimation,
+	pluginNoNestedMedia,
+	pluginOrder,
+	pluginSelectorNoEmpty,
+	pluginSelectorTagNoWithoutClass,
+	pluginStylistic,
+} from './plugins/_index.js';
+import {
+	stylelintAvoidErrors,
+	stylelintEnforceConventions,
+} from './rules/_index.js';
 
-export default processExports({ base,	parts });
+const base = {
+	languageOptions: {
+		directionality: {
+			block: 'top-to-bottom',
+			inline: 'left-to-right',
+		},
+	},
+};
+
+const parts = [
+	stylelintAvoidErrors,
+	stylelintEnforceConventions,
+	pluginDeclarationBlockNoIgnoredProperties,
+	pluginHighPerformanceAnimation,
+	pluginNoNestedMedia,
+	pluginOrder,
+	pluginSelectorNoEmpty,
+	pluginSelectorTagNoWithoutClass,
+	pluginStylistic,
+];
+
+export default processExports({ base, parts });
--- configurations/scss/index.ts
+++ configurations/scss/index.ts
@@ -1,5 +1,28 @@
+import postcssScss from 'postcss-scss';
+
 import { processExports } from '../../utils/helpers.js';
-import { base, parts } from './_settings.js';
+import {
+	pluginAtRuleNoChildren,
+	pluginScss,
+	pluginUseNesting,
+} from './plugins/_index.js';
+
+const base = {
+	customSyntax: postcssScss,
+	rules: {
+		'declaration-property-value-no-unknown': null,
+		'media-query-no-invalid': null,
+		'no-descending-specificity': null,
+		'no-invalid-position-at-import-rule': null,
+		'selector-no-invalid': null,
+	},
+};
+
+const parts = [
+	pluginAtRuleNoChildren,
+	pluginScss,
+	pluginUseNesting,
+];
 
 export default {
 	extends: ['./css.js'],
